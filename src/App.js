6eee26862b9ab9758aee8be594c169a590900149
qnrjs42 <qnrjs42@naver.com>
2020-12-19 22:06:43 +0900

    1-0. Introduction to useState

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [item, setItem] = useState(1);
+
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+
+  return (
+    <div>
+      <h1>Hello!!! {item}</h1>
+
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;

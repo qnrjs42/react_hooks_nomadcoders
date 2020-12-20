770a07b2350bdbbdf6a579f62874d08968920f71
qnrjs42 <qnrjs42@naver.com>
2020-12-20 12:56:57 +0900

    2-7. useNotification

--- src/useNotification.js
+++ src/useNotification.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const useNotification = (title, options) => { 
-  if(!("Notification" in window)) {
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
     return;
   }
 
   const fireNotif = () => {
-    if(Notification.permission !== 'granted') {
-      Notification.requestPermission().then(permission => {
-        if(permission === 'granted') {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
           new Notification(title, options);
         } else {
           return;
         }
-      })
+      });
     } else {
       new Notification(title, options);
     }
-  }
+  };
   return fireNotif;
-}
+};
 
-const App = () => {
-  const triggerNotif = useNotification("Can i ", {body: "i love "});
+const Notification = () => {
+  const triggerNotif = useNotification("Can i ", { body: "i love " });
 
   return (
     <div>
@@ -30,6 +30,6 @@ const App = () => {
       <button onClick={triggerNotif}>Button</button>
     </div>
   );
-}
+};
 
-export default App;
+export default Notification;

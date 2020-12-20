594b198d8cb21dcea7b698fb0938d6db806472a3
qnrjs42 <qnrjs42@naver.com>
2020-12-20 10:53:22 +0900

    2-4. useBeforeLeave

--- src/useBeforeLeave.js
+++ src/useBeforeLeave.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect } from "react";
 
 const useBeforeLeave = (onBefore) => {
   const handle = (event) => {
     // 마우스가 위로(탭을 닫으려고)갈 때 함수 실행
     const { clientY } = event;
-    if(clientY <= 0) onBefore();
-  }
-  
+    if (clientY <= 0) onBefore();
+  };
+
   useEffect(() => {
     if (typeof onBefore !== "function") {
       return;
@@ -14,12 +14,12 @@ const useBeforeLeave = (onBefore) => {
     document.addEventListener("mouseleave", handle);
     return () => document.removeEventListener("mouseleave", handle);
   }, []);
-}
+};
 
-const App = () => {
+const BeforeLeave = () => {
   const begForLife = () => console.log("Pls don't leave");
   useBeforeLeave(begForLife);
-  
+
   return (
     <div>
       <h1>Hello!!!</h1>
@@ -27,6 +27,6 @@ const App = () => {
       {/* <button onClick={}>UnProtect</button> */}
     </div>
   );
-}
+};
 
-export default App;
+export default BeforeLeave;
